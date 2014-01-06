528ef3642864a5215a77ea38957f9b4158cca8a2
Manuel Zambrano <mmzmm3z@gmail.com>
2014-01-06 15:54:37 -0430

    ordenando codigo

--- src/web/protected/components/Formatter.php
+++ src/web/protected/components/Formatter.php
@@ -1,42 +1,52 @@
 <?php
+/**
+ *
+ */
+class Formatter extends CApplicationComponent
+{
+    //***************************************************************************
+    // Initialization
+    //***************************************************************************
 
-class Formatter extends CApplicationComponent{   
-   //***************************************************************************
-   // Initialization
-   //***************************************************************************
-
-   /**
-    * Init method for the application component mode.
-    */
-   
+    /**
+     * Init method for the application component mode.
+     */
     public function init() {}
 
-
+    /**
+     *
+     */
     public function format_decimal($num,$decimales=3)
-    {   
-          if($num!=""){ 
-
-                $english_format_number2 = number_format($num, 10, ',', '.');
-                $numtext=strval($english_format_number2);
-                $position = strpos($numtext, ',');
-                $numsub = substr($numtext,0,$position+$decimales); 
-                return $numsub;
-        }else{
+    {
+        if($num!="")
+        {
+            $english_format_number2 = number_format($num, 10, ',', '.');
+            $numtext=strval($english_format_number2);
+            $position = strpos($numtext, ',');
+            $numsub = substr($numtext,0,$position+$decimales);
+            return $numsub;
+        }
+        else
+        {
             return "";
         }
     }
 
-    
-    public function format_date($fecha, $tipo=NULL) {
-
-        if($tipo==NULL){
-            
+    /**
+     *
+     */
+    public function format_date($fecha, $tipo=NULL)
+    {
+        if($tipo==NULL)
+        {
             $arrayFecha = explode("/", $fecha);
 
-            if (strlen($arrayFecha[0]) == 1) {
+            if(strlen($arrayFecha[0])==1)
+            {
                 $arrayFecha[0] = "0" . $arrayFecha[0];
             }
-            if (strlen($arrayFecha[1]) == 1) {
+            if(strlen($arrayFecha[1])==1)
+            {
                 $arrayFecha[1] = "0" . $arrayFecha[1];
             }
 
@@ -44,15 +54,12 @@ class Formatter extends CApplicationComponent{
             return $fechaFinal;
         }
         
-        if($tipo=='etelixPeru'){
-            
+        if($tipo=='etelixPeru')
+        {
             $arrayFecha = explode(" ", $fecha);
             return $arrayFecha[0];
-            
         }
-        
     }
-
 }
 
 ?>
