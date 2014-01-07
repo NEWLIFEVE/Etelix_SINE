4927f207ef7c55e94dfc212e8ad466270027f4ae
Manuel Zambrano <mmzmm3z@gmail.com>
2014-01-07 11:06:31 -0430

    mudada a DateManagement todos los metodos relacionados con fechas

--- src/web/protected/components/Reportes.php
+++ src/web/protected/components/Reportes.php
@@ -529,28 +529,5 @@ class Reportes extends CApplicationComponent
                                );
         return $termino_pago[$key];
     }
-
-    /** 
-     * Retorna la cantidad de dias de un mes
-     * @access protected
-     * @static
-     * @param date $fecha la fecha que se dira la cantidad de dias que tiene el mes
-     * @return int 
-     */
-    public static function howManyDays($fecha=null)
-    {
-        if(strpos($fecha,'-'))
-        {
-            $arrayFecha=explode('-',$fecha);
-        }
-        if(is_callable('cal_days_in_month'))
-        {
-            return cal_days_in_month(CAL_GREGORIAN, $arrayFecha[1], $arrayFecha[0]);
-        }
-        else
-        {
-            return date('d',mktime(0,0,0,$arrayFecha[1]+1,0,$arrayFecha[0]));
-        }
-    }
 }
 ?>
