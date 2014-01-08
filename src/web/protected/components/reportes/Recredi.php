c66c8967c317a8b53ef15af881530396223936fe
Angelo <angelo08121987@gmail.com>
2014-01-08 15:29:17 -0430

    comenzando a tomar vida recredi

--- src/web/protected/components/reportes/Recredi.php
+++ src/web/protected/components/reportes/Recredi.php
@@ -11,6 +11,8 @@
             $style_basic_head="style='border:1px solid black;background:#38BEE9;text-align:center;'";
             
             $carrierGroups=self::getAllGroups();
+            $seg=count($carrierGroups);
+            ini_set('max_execution_time', $seg);
             
             $reporte="<table><tr>";
             $reporte.="<td>RECREDI</td>";
@@ -41,16 +43,23 @@
             $reporte.="</tr>";
             foreach ($carrierGroups as $key => $group)
             {
+                $SOA=self::getSoaCarrier($group->id,$fecha);
+                $prov_fac_env=self::getProvisionsFact($group->id,$fecha,TRUE);
+                $prov_fac_rec=self::getProvisionsFact($group->id,$fecha,FALSE);
+                $prov_traf_env=self::getProvisionsTraf($group->id,$fecha,TRUE);
+                $prov_traf_rec=self::getProvisionsTraf($group->id,$fecha,FALSE);
+                $prov_disp_rec=self::getDisp($group->id,$fecha,TRUE);
+                $prov_disp_env=self::getDisp($group->id,$fecha,FALSE);
                     $reporte.="<tr $style_basic >";
                     $reporte.="<td $style_basic > $group->name </td>";
-                    $reporte.="<td $style_basic >" .self::getSoaCarrier($group->id,$fecha). "</td>";
+                    $reporte.="<td $style_basic > $SOA->amount </td>";
                     $reporte.="<td $style_basic > ULTIMO REGISTRO SOA </td>";
-                    $reporte.="<td $style_basic > CLIENTES " .self::getProvisionsFact($group->id,$fecha,TRUE). " </td>";
-                    $reporte.="<td $style_basic > PROVEEDORES " .self::getProvisionsFact($group->id,$fecha,FALSE). " </td>";
-                    $reporte.="<td $style_basic > CLIENTES REVENUE " .self::getProvisionsTraf($group->id,$fecha,TRUE). "</td>";
-                    $reporte.="<td $style_basic > PROVEEDORES COST " .self::getProvisionsTraf($group->id,$fecha,TRUE). "</td>";
-                    $reporte.="<td $style_basic > CLIENTES RECIBIDAS </td>";
-                    $reporte.="<td $style_basic > PROVEEDORES ENVIADAS </td>";
+                    $reporte.="<td $style_basic > $prov_fac_env->amount </td>";
+                    $reporte.="<td $style_basic > $prov_fac_rec->amount </td>";
+                    $reporte.="<td $style_basic > $prov_traf_env->amount </td>";
+                    $reporte.="<td $style_basic > $prov_traf_rec->amount </td>";
+                    $reporte.="<td $style_basic > $prov_disp_rec->amount </td>";
+                    $reporte.="<td $style_basic > $prov_disp_env->amount </td>";
                     $reporte.="<td $style_basic > BALANCE </td>";
                     $reporte.="</tr>";
                 }
@@ -73,8 +82,8 @@
         public static function getSoaCarrier($id,$date)
         {
             $sql="SELECT (p.amount-n.amount) AS amount
-                  FROM(SELECT CASE WHEN SUM(amount) IS NULL THEN 0 ELSE SUM(amount) END AS amount FROM accounting_document WHERE id_type_accounting_document IN(1,3) AND id_carrier={$id} AND issue_date<='{$date}') p,
-                      (SELECT CASE WHEN SUM(amount) IS NULL THEN 0 ELSE SUM(amount) END AS amount FROM accounting_document WHERE id_type_accounting_document IN(2,4) AND id_carrier={$id} AND issue_date<='{$date}') n";
+                  FROM(SELECT CASE WHEN SUM(amount) IS NULL THEN 0 ELSE SUM(amount) END AS amount FROM accounting_document WHERE id_type_accounting_document IN(1,3) AND id_carrier ={$id} AND issue_date<='{$date}') p,
+                      (SELECT CASE WHEN SUM(amount) IS NULL THEN 0 ELSE SUM(amount) END AS amount FROM accounting_document WHERE id_type_accounting_document IN(2,4) AND id_carrier ={$id} AND issue_date<='{$date}') n";
             return AccountingDocument::model()->findBySql($sql);
         }
         /**
@@ -86,10 +95,10 @@
          */
         public static function getProvisionsFact($id,$date,$type=TRUE)
         {
-            if($type) $condicion="12";
-            else      $condicion="13";
+            if($type) $type_id="12";
+            else      $type_id="13";
             
-            $sql="SELECT CASE WHEN SUM(amount) IS NULL THEN 0 ELSE SUM(amount) END AS amount FROM accounting_document WHERE id_type_accounting_document ={$type} AND id_carrier={$id} AND issue_date<='{$date}'";
+            $sql="SELECT CASE WHEN SUM(amount) IS NULL THEN 0 ELSE SUM(amount) END AS amount FROM accounting_document WHERE id_type_accounting_document ={$type_id} AND id_carrier={$id} AND issue_date<='{$date}'";
             return AccountingDocument::model()->findBySql($sql);
         }
         /**
@@ -101,9 +110,16 @@
          */
         public static function getProvisionsTraf($id,$date,$type=TRUE)
         {
-            if($type) $condicion="12";
-            else      $condicion="13";
-            $sql="SELECT CASE WHEN SUM(amount) IS NULL THEN 0 ELSE SUM(amount) END AS amount FROM accounting_document WHERE id_type_accounting_document ={$type} AND id_carrier={$id} AND issue_date<='{$date}'";
+            if($type) $type_id="12";
+            else      $type_id="13";
+            $sql="SELECT CASE WHEN SUM(amount) IS NULL THEN 0 ELSE SUM(amount) END AS amount FROM accounting_document WHERE id_type_accounting_document ={$type_id} AND id_carrier={$id} AND issue_date<='{$date}'";
+            return AccountingDocument::model()->findBySql($sql);
+        }
+        public static function getDisp($id,$date,$type=TRUE)
+        {
+            if($type) $type_id="5";
+            else      $type_id="6";
+            $sql="SELECT CASE WHEN SUM(amount) IS NULL THEN 0 ELSE SUM(amount) END AS amount FROM accounting_document WHERE id_type_accounting_document ={$type_id} AND id_carrier={$id} AND issue_date<='{$date}'";
             return AccountingDocument::model()->findBySql($sql);
         }
             
