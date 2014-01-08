f3feefd308c2a009c581bb17c1abd4d220662971
Manuel Zambrano <mmzmm3z@gmail.com>
2014-01-08 14:29:13 -0430

    cambio de conexion a base de datos

--- src/web/protected/config/console.php
+++ src/web/protected/config/console.php
@@ -18,7 +18,7 @@ return array(
             'class'=>"application.components.Provisions",
         ),		
 		'db'=>array(
-            'connectionString'=>'pgsql:host=172.16.17.190;port=5432;dbname=sori',
+            'connectionString'=>'pgsql:host=172.16.17.190;port=5432;dbname=test_sori',
 			'emulatePrepare'=>true,
 			'username'=>'postgres',
             'password'=>'123',
