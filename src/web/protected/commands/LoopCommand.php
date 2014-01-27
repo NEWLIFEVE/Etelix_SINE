c0acfbf47b7fda467d4b72a1c13c9a3661a22e80
Manuel Zambrano <mmzmm3z@gmail.com>
2014-01-27 17:07:09 -0430

    ambiente de pruebas locales

--- src/web/protected/commands/LoopCommand.php
+++ src/web/protected/commands/LoopCommand.php
@@ -6,8 +6,8 @@ class LoopCommand extends CConsoleCommand
 {
 	public function run($args)
 	{
-		$date='2013-10-02';
-		$final='2014-01-07';
+		$date='2013-07-02';
+		$final='2014-01-24';
 		while ($date <= $final)
 		{
 			Yii::app()->provisions->run($date);
