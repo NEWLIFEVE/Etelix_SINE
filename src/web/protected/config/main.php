fdc7d2607d6c5d45d64729baf887b49f5d7576cc
Manuel Zambrano <mmzmm3z@gmail.com>
2014-03-06 16:34:30 -0430

    entorno de desarrollo

--- src/web/protected/config/main.php
+++ src/web/protected/config/main.php
@@ -63,7 +63,7 @@ return array(
 			),
 		),
         'db'=>array(
-            'connectionString'=>'pgsql:host=172.16.17.190;port=5432;dbname=soa_sori',
+            'connectionString'=>'pgsql:host=172.16.17.190;port=5432;dbname=sori',
             'emulatePrepare'=>true,
             'username'=>'postgres',
             'password'=>'123',
