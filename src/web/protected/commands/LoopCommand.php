df84ec5f83f8a64a4347a7386f932b651b035f6c
Manuel Zambrano <mmzmm3z@gmail.com>
2014-04-01 00:13:49 -0430

    soporta entrada de grupo null

--- src/web/protected/commands/LoopCommand.php
+++ src/web/protected/commands/LoopCommand.php
@@ -6,9 +6,10 @@ class LoopCommand extends CConsoleCommand
 {
 	public function run($args)
 	{
+		$carrier=null;
 		$date=$args[0];
 		$final=$args[1];
-		$carrier=$args[2];
+		if(isset($args[2])) $carrier=$args[2];
 		while ($date <= $final)
 		{
 			Yii::app()->provisions->run($date,$carrier);
