23e53ea1fd3939cbc90258e46940c77f6d4ef09a
Manuel Zambrano <mmzmm3z@gmail.com>
2014-04-01 00:13:23 -0430

    cambios en entornos de trsbajo

--- src/web/console.php
+++ src/web/console.php
@@ -2,23 +2,34 @@
 date_default_timezone_set('America/Caracas');
 //Definimos nuestro servidor de produccion
 define('SERVER_NAME_PROD','s1248-101');
+//Definimos el directorio de desarrollo
+define('DIRECTORY_NAME_PRE_PROD','dev_sine');
 //Obtenemos el nombre del servidor actual
 $server=gethostname();
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../../../yii/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/console.php';
-
-switch ($server)
+if($server==SERVER_NAME_PROD)
 {
-	case SERVER_NAME_PROD:
-		defined('YII_DEBUG') or define('YII_DEBUG',false);
-		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
-		break;
-	default:
+	$server=dirname(__FILE__);
+	$nuevo=explode(DIRECTORY_SEPARATOR,$server);
+	$num=count($nuevo);
+	if($nuevo[$num-3]==DIRECTORY_NAME_PRE_PROD)
+	{
 		defined('YII_DEBUG') or define('YII_DEBUG',true);
 		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-		break;
+	}
+	else
+	{
+		defined('YII_DEBUG') or define('YII_DEBUG',false);
+		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
+	}
 }
+else
+{
+	defined('YII_DEBUG') or define('YII_DEBUG',true);
+	defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+}
+
+$yii=dirname(__FILE__).'/../../../yii/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/console.php';
 
 require_once($yii);
 Yii::createConsoleApplication($config)->run();
